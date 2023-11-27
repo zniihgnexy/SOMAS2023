bcbada848948dddef39af47fb4aff2609f3c53ce
zniihgnexy <xz2723@ic.ac.uk>
2023-11-27 19:16:49 +0000

    simple logic without detailed algoriehm

---
+++ internal/clients/team 4/honest_matrix.go
@@ -0,0 +1,19 @@
+/* honest matrix:
+update points:
+	Deliberative democracy:
+	voting: the non-binding vote we can see the total voting pool and the ranking of this,
+	if the real vote after that came out with a  surpring result (not the same as the original)
+	decrease the honest of all the agent on the bike
+	pedal: get total pedaling forces(?), compare with the voting results, if no match then reduce
+
+	Leadership:
+	leadership can decide to kickout and join. the rest still can vote, but the leader make the
+	decision. if the ranking and the final result not match, then tell a lie (?)
+	not democrary anyway
+
+	Dictatorship:
+	the leader decide everything. if the leader choose the lootbox has different colors
+	with itself, then telling a lie. if kickout a same color agent, then telling a lie.
+	if kickout a same oclor then tell a lie (not a good leader)
+
+*/
