8be1fd426769aba31270def59343be5101492c80
zniihgnexy <xz2723@ic.ac.uk>
2023-11-27 19:52:18 +0000

    structure

--- internal/clients/team 4/honest_matrix.go
+++ internal/clients/team 4/honest_matrix.go
@@ -17,3 +17,57 @@ update points:
 	if kickout a same oclor then tell a lie (not a good leader)
 
 */
+
+package team4
+
+import (
+	"github.com/google/uuid"
+)
+
+// HonestyRecord represents the probability of an agent being honest in a specific context.
+type HonestyRecord struct {
+	HonestyProbability float64
+	Context            string
+}
+
+type HonestyMatrix struct {
+	Records map[uuid.UUID][]HonestyRecord
+}
+
+type ActionData struct {
+	// define the actions
+	// votingResult, leadershipDecision, etc
+}
+
+// NewHonestyMatrix creates a new HonestyMatrix.
+func NewHonestyMatrix() *HonestyMatrix {
+	return &HonestyMatrix{
+		Records: make(map[uuid.UUID][]HonestyRecord),
+	}
+}
+
+// UpdateHonesty updates the honesty record for an agent.
+func (hm *HonestyMatrix) UpdateHonesty(agentID uuid.UUID, probability float64, context string) {
+	hm.Records[agentID] = append(hm.Records[agentID], HonestyRecord{HonestyProbability: probability, Context: context})
+}
+
+// GetRecords returns all honesty records for a given agent.
+func (hm *HonestyMatrix) GetRecords(agentID uuid.UUID) []HonestyRecord {
+	return hm.Records[agentID]
+}
+
+/*
+func (hm *HonestyMatrix) CalculateHonestyBasedOnActions(agentID uuid.UUID, actionData ActionData) {
+	// implement algorithm
+	bikeID := hm.GetBike()
+	newHonestyValue := 0.0 // fixed or changing
+	fellowBikers := hm.GameState.GetMegaBikes()[bikeID].GetAgents()
+	for _, agentID := range fellowBikers {
+		actionData := gatherActionDataForAgent(agentID) // Implement this function based on your game logic
+		honestyMatrix.CalculateHonestyBasedOnActions(agentID, actionData)
+	}
+
+	// Update the honesty matrix with the new value
+	hm.UpdateHonesty(agentID, newHonestyValue, "Description of the context")
+}
+*/
