c5fa1844fc80d6cbf63f24aa1709b2424844482b
Yanzhou Jin <90556741+Yanzhou-Jin@users.noreply.github.com>
2023-12-02 10:53:52 +0000

    Update baseline.go

--- internal/clients/team_4/baseline.go
+++ internal/clients/team_4/baseline.go
@@ -5,9 +5,13 @@ import (
 	"SOMAS2023/internal/common/physics"
 	"SOMAS2023/internal/common/utils"
 	"SOMAS2023/internal/common/voting"
+	"fmt"
+
+	//"go/printer"
 	"math"
 	"math/rand"
 	"sort"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -18,13 +22,13 @@ type IBaselineAgent interface {
 
 type BaselineAgent struct {
 	objects.BaseBiker
-	currentBike   *objects.MegaBike
-	targetLootBox objects.ILootBox
+	currentBike *objects.MegaBike
 }
 
 // DecideAction only pedal
 func (agent *BaselineAgent) DecideAction() objects.BikerAction {
 	return objects.Pedal
+
 }
 
 // DecideForces randomly based on current energyLevel
@@ -40,6 +44,8 @@ func (agent *BaselineAgent) DecideForces(direction uuid.UUID) {
 	} else {
 		randomBreakForce = 0
 	}
+	fmt.Print("energyLevel", energyLevel)
+	fmt.Println("randomPedalForce", randomPedalForce)
 
 	// 因为force是一个struct,包括pedal, brake,和turning，因此需要一起定义，不能够只有pedal
 	forces := utils.Forces{
@@ -63,14 +69,6 @@ func (agent *BaselineAgent) DecideJoining(pendingAgents []uuid.UUID) map[uuid.UU
 	return decision
 }
 
-func (agent *BaselineAgent) ProposeDirection() utils.Coordinates {
-	e := agent.decideTargetLootBox(agent.GetGameState().GetLootBoxes())
-	if e != nil {
-		panic("unexpected error!")
-	}
-	return agent.targetLootBox.GetPosition()
-}
-
 func (agent *BaselineAgent) FinalDirectionVote(proposals []uuid.UUID) voting.LootboxVoteMap {
 	boxesInMap := agent.GetGameState().GetLootBoxes()
 	boxProposed := make([]objects.ILootBox, len(proposals))
@@ -81,58 +79,44 @@ func (agent *BaselineAgent) FinalDirectionVote(proposals []uuid.UUID) voting.Loo
 	if e != nil {
 		panic("unexpected error!")
 	}
+	agent.calc_Reputation_value(0.1, 0.5, 0.8)
+	//agent.Reputation_loop()
 	return rank
 }
 
-func (agent *BaselineAgent) DecideAllocation() voting.IdVoteMap {
-	currentBike := agent.GetGameState().GetMegaBikes()[agent.currentBike.GetID()]
-	rank, e := agent.rankAgentsReputation(currentBike.GetAgents())
-	if e != nil {
-		panic("unexpected error!")
-	}
-	return rank
-}
-
-// decideTargetLootBox find closest lootBox
-func (agent *BaselineAgent) decideTargetLootBox(lootBoxes map[uuid.UUID]objects.ILootBox) error {
-
-	agentLocation := agent.GetLocation() //agent location
-	shortestDistance := math.MaxFloat64  //最短距离一开始设置为正无穷
-
-	for _, lootbox := range lootBoxes { //遍历每一个lootbox
-		lootboxLocation := lootbox.GetPosition()
-		distance := physics.ComputeDistance(agentLocation, lootboxLocation)
-		// try to calculate if now the energy is low
-		/*
-			if agent.GetEnergyLevel() < originalEnergy * 0.1{
-				if distance < shortestDistance && agent.GetColour() == lootbox.GetColour(){
-					shortestDistance = distance
-					agent.targetLootBox = lootbox
-				}
-			}else if distance < shortestDistance {
-				shortestDistance = distance
-				agent.targetLootBox = lootbox
-			}
-		*/
-		if distance < shortestDistance {
-			shortestDistance = distance
-			agent.targetLootBox = lootbox
-		}
-	}
-	return nil
-}
-
-// rankTargetProposals rank by distance
 func (agent *BaselineAgent) rankTargetProposals(proposedLootBox []objects.ILootBox) (map[uuid.UUID]float64, error) {
-	currentBike := agent.GetGameState().GetMegaBikes()[agent.currentBike.GetID()]
+
+	currentMegaBike := agent.GetGameState().GetMegaBikes()[agent.GetBike()]
+	fellowBikers := currentMegaBike.GetAgents()
+
 	// sort lootBox by distance
 	sort.Slice(proposedLootBox, func(i, j int) bool {
-		return physics.ComputeDistance(currentBike.GetPosition(), proposedLootBox[i].GetPosition()) < physics.ComputeDistance(currentBike.GetPosition(), proposedLootBox[j].GetPosition())
+		return physics.ComputeDistance(currentMegaBike.GetPosition(), proposedLootBox[i].GetPosition()) < physics.ComputeDistance(currentMegaBike.GetPosition(), proposedLootBox[j].GetPosition())
 	})
 	rank := make(map[uuid.UUID]float64)
+	ranksum := make(map[uuid.UUID]float64)
+	totalsum := float64(0)
+	totaloptions := len(proposedLootBox)
+
 	for i, lootBox := range proposedLootBox {
-		rank[lootBox.GetID()] = float64(i)
+		for _, fellows := range fellowBikers {
+			if fellows.GetColour() == lootBox.GetColour() {
+				ranksum[lootBox.GetID()] += float64(totaloptions-i) / 1.2
+				totalsum += float64(totaloptions-i) / 1.2
+			}
+		}
+		if lootBox.GetColour() == agent.GetColour() {
+			ranksum[lootBox.GetID()] += 2 * float64(totaloptions-i)
+			totalsum += 2 * float64(totaloptions-i)
+		} else {
+			ranksum[lootBox.GetID()] += float64(totaloptions-i) / float64(totaloptions)
+			totalsum += float64(totaloptions-i) / float64(totaloptions)
+		}
 	}
+	for _, lootBox := range proposedLootBox {
+		rank[lootBox.GetID()] = ranksum[lootBox.GetID()] / totalsum
+	}
+
 	return rank, nil
 }
 
@@ -145,4 +129,144 @@ func (agent *BaselineAgent) rankAgentsReputation(agentsOnBike []objects.IBaseBik
 		rank[agent.GetID()] = float64(i)
 	}
 	return rank, nil
+}
+
+func (agent *BaselineAgent) DecideGovernance() voting.GovernanceVote {
+	rank := make(map[utils.Governance]float64)
+	rank[utils.Democracy] = 1
+	rank[utils.Leadership] = 0
+	rank[utils.Dictatorship] = 0
+	rank[utils.Invalid] = 0
+	//for i := utils.Democracy; i <= utils.Invalid; i++ {
+	//  rank[i] = 0.25
+	//}
+	fmt.Println(rank)
+	return rank
+}
+
+// ///////////////////////////////////////////////
+// Yanzhou's  Changes (the function is called in line 82)
+// ----> 82 agent.calc_Reputation_value(0.1, 0.5, 0.8)
+//
+// Description:
+// 1. it returns value of the reputation of a agent.
+// 2. it have to be tuned and normalized in the reputation matrix
+// 3. now, we can calculate the value of the reputation value, but cannot calculate the matrix because datatype issue.
+//
+// ISSUE:
+// 1. The 'calc_Reputation_value' function can only calculate values of "*BaselineAgent", can not work for "*IBaseBiker"
+// 2. To generate the reputation, we can enumerate all agents in the system, and calculate reputations for each of them
+//    But as I enumerate all agents, (in line 196) the varible otherAgent is "*IBaseBiker",
+//    it can only be called in the Gameloop.go, I have no access to it
+//    Thus, I don't think there is a way to calculate the reputation matrix in this file
+// ///////////////////////////////////////////////
+
+// /////////////////////////
+// func (agent *BaselineAgent) GetReputation() map[uuid.UUID]float64 {
+// 	reputation := make(map[uuid.UUID]float64)
+// 	print("1232132213123212312\n")
+// 	return reputation
+// }
+
+func (agent *BaselineAgent) leaving() float64 { //get input
+	rand.Seed(time.Now().UnixNano())
+	randomBit := float64(rand.Intn(2))
+	return randomBit
+}
+
+func (agent *BaselineAgent) energy_consumption() float64 {
+	// Generate a random energy consumption for the current loop
+	consumption := rand.Float64() * agent.GetEnergyLevel()
+	return consumption
+}
+
+func (agent *BaselineAgent) selected_Lootbox_distance() float64 {
+	// Randomly select a lootbox as the selected lootbox for the current loop
+	lootBoxes := agent.GetGameState().GetLootBoxes()
+
+	var distance float64
+	for _, lootBox := range lootBoxes {
+		// Implement specific logic here to calculate the distance between agent and LootBox
+		distance = physics.ComputeDistance(agent.GetLocation(), lootBox.GetPosition())
+		if rand.Float64() < 0.5 {
+			break
+		}
+	}
+	return distance
+}
+
+// func (agent *BaselineAgent) Reputation_loop() map[uuid.UUID]float64 {
+// 	reputation := make(map[uuid.UUID]float64)
+// 	megaBikes := agent.GetGameState().GetMegaBikes()
+
+// 	for _, bike := range megaBikes {
+// 		// Get all agents on MegaBike
+// 		fellowBikers := bike.GetAgents()
+
+// 		// Iterate over each agent on MegaBike, generate reputation assessment
+// 		for _, otherAgent := range fellowBikers {
+// 			// Exclude self
+// 			test := otherAgent.GetID() //nolint
+// 			if test == agent.GetID() {
+// 				continue
+// 			}
+// 			otherAgent.GetReputation()
+
+// 		}
+// 	}
+// 	return reputation
+// }
+
+func (agent *BaselineAgent) calc_Reputation_value(b, c, d float64) float64 {
+	reputation_value := 0.0
+	// Get all LootBoxes on the field
+	lootBoxes := agent.GetGameState().GetLootBoxes()
+
+	// 1. Minimum distance and selected target value between agent and all LootBoxes
+	var minDistance float64 = math.Inf(1)
+	i := 0
+	for _, lootBox := range lootBoxes {
+		// Implement specific logic here to calculate the distance between agent and LootBox
+		distance := physics.ComputeDistance(agent.GetLocation(), lootBox.GetPosition())
+
+		// Check if it is a new minimum distance
+		if distance < minDistance {
+			minDistance = distance
+		}
+
+		// If the current loop index equals the specified index a, record the distance to the selected LootBox
+		i = i + 1
+	}
+
+	// Calculate distance_Reputation
+	distance_Reputation := 1 - (b * agent.selected_Lootbox_distance() / minDistance)
+
+	// Here you can use minDistance, selected_Lootdistance, and distance_Reputation for further logic
+	print("Agent " + agent.GetID().String() + " Reputation = \n")
+	print("distance_Reputation: \t", distance_Reputation)
+	print("\n")
+
+	// 2. EnergyLevel of the agent in this and the previous loop
+	// Get the current EnergyLevel
+	currentEnergyLevel := agent.GetEnergyLevel()
+	currentConsumption := agent.energy_consumption()
+
+	energy_Reputation := currentEnergyLevel - c*currentConsumption
+
+	print("energy_Reputation: \t", energy_Reputation)
+	print("\n")
+
+	// Get whether the agent leaves the bike this round 0 means no, 1 means yes
+	leave_value := agent.leaving()
+	Leave_Reputation := 1 - d*leave_value
+	print("Leave_Reputation: \t", Leave_Reputation)
+	print("\n")
+
+	///
+	///////////////
+	reputation_value = Leave_Reputation*energy_Reputation - distance_Reputation
+	print("reputation_value: \t", reputation_value)
+	print("\n")
+	print("\n")
+	return reputation_value
 }
